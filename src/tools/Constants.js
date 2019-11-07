71be7f81f80abad83b718ab3ed2b155cb57682a9
nathan.bonnet@epitech.eu <nathan.bonnet@epitech.eu>
2019-11-07 14:02:28 +0100

    deploy_test

--- src/tools/Constants.js
+++ src/tools/Constants.js
@@ -9,7 +9,7 @@ class Constants {
     this.PORT = 3000;
     this.PORT_DEV = 3080;
 
-    this.DB_URI = 'mongodb://localhost:27017/imBored';
+    this.DB_URI = 'mongodb://localhost/imBored';
     this.DB_URI_DEV = 'mongodb://localhost:27017/imBored-dev';
 
     this.SECRET = 'thisIs4SecretKey@';
